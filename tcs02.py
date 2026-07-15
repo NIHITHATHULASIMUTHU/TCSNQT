45d4c92ba5bdb1d21f49d0c7fd8416f17b886f85
T.NIHITHA <nihitha0426@gmail.com>
2026-07-15 15:47:39 +0000

    02

---
+++ tcs02.py
@@ -0,0 +1,12 @@
+#check prime number
+n = int(input())
+
+if n <= 1:
+    print("not a prime")
+else:
+    for i in range(2, n):
+        if n % i == 0:
+            print("not a prime")
+            break
+    else:
+        print("prime")
